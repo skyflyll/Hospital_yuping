9824fd244238442d0dae4e38a23e2ac988e7b2fe
May in summer <linlang_1994@outlook.com>
2018-05-03 16:01:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -14,9 +14,7 @@ module.exports = {
       '/api': {
         target: 'http://127.0.0.1:8088/',
         changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/'
-        }
+        pathRewrite: { '^/api': '/' },
       }
     },
     // proxyTable: {},
